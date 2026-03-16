3520c5d5348addea5cc2288e539d06e5235a3079
KS1 <shinehome888@gmail.com>
2026-03-16 02:15:57 -0700

    Delete src directory

--- src/models/Document.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const DocumentSchema = new mongoose.Schema({
-  smeId: { type: mongoose.Schema.Types.ObjectId, ref: 'SMEProfile', required: true },
-  documentType: { type: String, enum: ['NATIONAL_ID', 'BUSINESS_CERTIFICATE'], required: true },
-  fileUrl: { type: String, required: true }
-}, { timestamps: true });
-
-module.exports = mongoose.model('Document', DocumentSchema);
